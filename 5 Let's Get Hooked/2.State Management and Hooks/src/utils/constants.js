2a466febe3e68a2549627ac1163818d56b12d8e6
Somasekhar Agoli <117739168+Somasekhar9666@users.noreply.github.com>
2024-12-14 20:07:24 +0530

    Delete 5 Let's Get Hooked/2.State Management and Hooks/src directory

--- 5 Let's Get Hooked/2.State Management and Hooks/src/utils/constants.js
+++
@@ -1,5 +0,0 @@
-
-
-export const SWIGGY_CDN_IMAGE_URL="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660"
-
-export const LOGO_URL="https://png.pngtree.com/png-clipart/20220911/original/pngtree-chef-restaurant-logo-png-image_8538757.png"

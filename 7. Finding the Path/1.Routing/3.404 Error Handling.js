3104dd1890a537f7ab3dbfe4b2952acd451e3723
somasekhar <somasekharagolie1@gamil.com>
2025-01-12 00:06:34 +0530

    its about routing

---
+++ 7. Finding the Path/1.Routing/3.404 Error Handling.js
@@ -0,0 +1,55 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import  Header from "./src/components/Header"
+import Body from "./src/components/Body";
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
+
+
+
+
+
+
+const WholeApp= ()=> {
+           return ( 
+           <div className="app">
+                <Header/> 
+                <Body/>
+            </div>
+        );
+    };
+
+
+
+
+
+
+const appRouter=createBrowserRouter([
+    {
+        path:"/",
+        element:<WholeApp/>,
+        errorElement:<Error/>
+    },
+                                                                                    //defining Routes so that when url changes the specified compomnet is renderd
+    {
+        path:"/about",                  
+        element:<About/>,
+        errorElement:<Error/>
+    }
+
+])
+
+
+
+
+// Render  paragraph(a unifiefd React Component)
+const root = ReactDOM.createRoot(document.getElementById("root1"));
+
+
+
+root.render(<RouterProvider router={appRouter}/>);                                                                  // intially rendering the router through the react provider 
+
+
+
+
+
+
